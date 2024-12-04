cb36330e90e21af110805970b26d5a98f5f5ddea
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-12-04 20:39:03 +0100

    Calculator working

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.css']
+  styleUrls: ['./calculadora.component.css'],
 })
 export class CalculadoraComponent {
+  currentInput: string = '0';
+  buttons: string[] = [
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    '*',
+    '1',
+    '2',
+    '3',
+    '-',
+    'C',
+    '0',
+    '+',
+    '=',
+  ];
 
+  buttonRows: string[][] = [];
+
+  constructor() {
+    this.buttonRows = this.groupButtons(this.buttons, 4);
+  }
+
+  groupButtons(buttons: string[], size: number): string[][] {
+    const rows = [];
+    for (let i = 0; i < buttons.length; i += size) {
+      rows.push(buttons.slice(i, i + size));
+    }
+    return rows;
+  }
+
+  handleButtonClick(button: string): void {
+    if (button === 'C') {
+      this.currentInput = '0';
+    } else if (button === '=') {
+      try {
+        this.currentInput = eval(this.currentInput);
+      } catch {
+        this.currentInput = 'Error';
+      }
+    } else {
+      this.currentInput =
+        this.currentInput === '0' ? button : this.currentInput + button;
+    }
+  }
 }
--- src/app/components/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/components/tic-tac-toe/tic-tac-toe.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tic-tac-toe',
   templateUrl: './tic-tac-toe.component.html',
-  styleUrls: ['./tic-tac-toe.component.css']
+  styleUrls: ['./tic-tac-toe.component.css'],
 })
 export class TicTacToeComponent {
-
+  matrix: string[][] = [];
 }
