9084017bc073877701cff5e24d1a10df1fcbcf0d
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-12-04 17:58:02 +0100

    Linked tic-tac-toe and calculadora with main index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { TicTacToeComponent } from './components/tic-tac-toe/tic-tac-toe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "tic-tac-toe", component: TicTacToeComponent},
+  {path: "calculadora", component: CalculadoraComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
