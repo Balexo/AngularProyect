d0bafca049a58ab08f3862744af948b1fabfd5ec
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-12-05 19:52:28 +0100

    Tic-tac-toe component finished pending for winner logic

--- src/app/components/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/components/tic-tac-toe/tic-tac-toe.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./tic-tac-toe.component.css'],
 })
 export class TicTacToeComponent {
-  matrix: string[][] = [];
+  matrix: string[][] = this.matrixSize(5);
+  clickCounter: number = 0;
+
+  winner: string = '';
+
+  matrixSize(size: number): string[][] {
+    const matrix: string[][] = [];
+    for (let i = 0; i < size; i++) {
+      const row: string[] = [];
+      for (let j = 0; j < size; j++) {
+        row.push(' ');
+      }
+      matrix.push(row);
+    }
+    return matrix;
+  }
+
+  counterClick(rowIndex: number, columnIndex: number) {
+    this.clickCounter++;
+    if (this.matrix[rowIndex][columnIndex] === ' ') {
+      this.matrix[rowIndex][columnIndex] =
+        this.clickCounter % 2 === 0 ? 'X' : 'O';
+    }
+  }
 }
