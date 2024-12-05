fd1e95766f06494a959eb3ada9d534e774128478
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-12-05 19:13:18 +0100

    New page structure with home in the left and tic-tac-toe and calculator in the right

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { CalculadoraComponent } from './components/calculadora/calculadora.compo
 import { TicTacToeComponent } from './components/tic-tac-toe/tic-tac-toe.component';
 
 const routes: Routes = [
-  {path: "tic-tac-toe", component: TicTacToeComponent},
-  {path: "calculadora", component: CalculadoraComponent}
+  { path: 'tic-tac-toe', component: TicTacToeComponent },
+  { path: 'calculadora', component: CalculadoraComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
-
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    TicTacToeComponent,
-    CalculadoraComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, TicTacToeComponent, CalculadoraComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HomeComponent, TicTacToeComponent, CalculadoraComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
